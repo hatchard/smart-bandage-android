1d15388b6ca6b650d67b59efffc99306e781daac
Jared Cuglietta <jbcuglietta@gmail.com>
2016-02-28 12:09:24 -0700

    Should now display multiple devices, and status

--- app/src/main/java/com/example/jared/smart_bandage_android/ConnectedDevicesActivity.java
+++ app/src/main/java/com/example/jared/smart_bandage_android/ConnectedDevicesActivity.java
@@ -8,15 +8,28 @@ import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ExpandableListView;
+import android.widget.ListView;
 import android.widget.SimpleExpandableListAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,28 +38,40 @@ import java.util.List;
 
 public class ConnectedDevicesActivity extends AppCompatActivity {
     private final static String TAG = ConnectedDevicesActivity.class.getSimpleName();
+
+    public final static String ACTION_GATT_CONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+
+    private ConnectionListAdapter connectionListAdapter;
     public static HashMap<String,SmartBandage> deviceList;
     public static String DEVICE_LIST ="deviceList";
-    private ExpandableListView deviceListview;
+    private ListView deviceListview;
     private BluetoothAdapter bluetoothAdapter;
-    private final String LIST_NAME = "NAME";
-    private final String LIST_UUID = "UUID";
-    private ArrayList<ArrayList<BluetoothGattCharacteristic>> mGattCharacteristics =
-            new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
+    private HashMap<String,SmartBandage> deviceConnectionStatus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connected_devices);
         deviceList = (HashMap<String,SmartBandage>) getIntent().getSerializableExtra(DEVICE_LIST);
-        deviceListview = (ExpandableListView)findViewById(R.id.listView);
-
+        deviceListview = (ListView)findViewById(R.id.listView);
+        deviceConnectionStatus = new HashMap<String,SmartBandage>();
         BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
-
+        connectionListAdapter = new ConnectionListAdapter(this);
+        deviceListview.setAdapter(connectionListAdapter);
         for(String key : deviceList.keySet()){
-            Log.d(TAG,"Attempting Connection to Device " + key);
+            Log.d(TAG, "Attempting Connection to Device " + key);
             BluetoothDevice device =  bluetoothAdapter.getRemoteDevice(key);
-            device.connectGatt(this,false,mGattCallback);
+            device.connectGatt(this,true,mGattCallback);
         }
     }
 
@@ -55,13 +80,18 @@ public class ConnectedDevicesActivity extends AppCompatActivity {
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             String intentAction;
             if (newState == BluetoothProfile.STATE_CONNECTED) {
+                SmartBandage sm = new SmartBandage(gatt,SmartBandage.CONNECTED);
                 Log.i(TAG, "Connected to GATT server.");
                 // Attempts to discover services after successful connection.
+                processResult(sm);
                 Log.i(TAG, "Attempting to start service discovery:" +
                         gatt.discoverServices());
 
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 Log.i(TAG, "Disconnected from GATT server.");
+                SmartBandage sm = new SmartBandage(gatt,SmartBandage.DISCONNECTED);
+                processResult(sm);
+
 
             }
         }
@@ -69,7 +99,7 @@ public class ConnectedDevicesActivity extends AppCompatActivity {
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             if (status == BluetoothGatt.GATT_SUCCESS) {
-                displayGattServices(gatt.getServices());
+
             } else {
                 Log.w(TAG, "onServicesDiscovered received: " + status);
             }
@@ -91,57 +121,56 @@ public class ConnectedDevicesActivity extends AppCompatActivity {
         }
     };
 
-    private void displayGattServices(List<BluetoothGattService> gattServices) {
-        if (gattServices == null) return;
-        String uuid = null;
-        String unknownServiceString = getResources().getString(R.string.unknown_service);
-        String unknownCharaString = getResources().getString(R.string.unknown_characteristic);
-        ArrayList<HashMap<String, String>> gattServiceData = new ArrayList<HashMap<String, String>>();
-        ArrayList<ArrayList<HashMap<String, String>>> gattCharacteristicData
-                = new ArrayList<ArrayList<HashMap<String, String>>>();
-        mGattCharacteristics = new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
 
-        // Loops through available GATT Services.
-        for (BluetoothGattService gattService : gattServices) {
-            HashMap<String, String> currentServiceData = new HashMap<String, String>();
-            uuid = gattService.getUuid().toString();
-            currentServiceData.put(
-                    LIST_NAME, SampleGattAttributes.lookup(uuid, unknownServiceString));
-            currentServiceData.put(LIST_UUID, uuid);
-            gattServiceData.add(currentServiceData);
+    private class ConnectionListAdapter extends ArrayAdapter<SmartBandage> {
 
-            ArrayList<HashMap<String, String>> gattCharacteristicGroupData =
-                    new ArrayList<HashMap<String, String>>();
-            List<BluetoothGattCharacteristic> gattCharacteristics =
-                    gattService.getCharacteristics();
-            ArrayList<BluetoothGattCharacteristic> charas =
-                    new ArrayList<BluetoothGattCharacteristic>();
+        public ConnectionListAdapter(Context context) {
+            super(context, 0);
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup){
 
-            // Loops through available Characteristics.
-            for (BluetoothGattCharacteristic gattCharacteristic : gattCharacteristics) {
-                charas.add(gattCharacteristic);
-                HashMap<String, String> currentCharaData = new HashMap<String, String>();
-                uuid = gattCharacteristic.getUuid().toString();
-                currentCharaData.put(
-                        LIST_NAME, SampleGattAttributes.lookup(uuid, unknownCharaString));
-                currentCharaData.put(LIST_UUID, uuid);
-                gattCharacteristicGroupData.add(currentCharaData);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext())
+                        .inflate(R.layout.connection_status_layout, viewGroup, false);
             }
-            mGattCharacteristics.add(charas);
-            gattCharacteristicData.add(gattCharacteristicGroupData);
+            final SmartBandage smartBandage = getItem(position);
+
+            TextView deviceName = (TextView) convertView.findViewById(R.id.connectionName);
+            deviceName.setText(smartBandage.getBandageName());
+
+            TextView deviceAddress = (TextView) convertView.findViewById(R.id.connectionAddress);
+            deviceAddress.setText(smartBandage.getBandageAddress());
+
+            TextView deviceConnectionStatus = (TextView) convertView.findViewById(R.id.connectionStatus);
+            if (smartBandage.getBandageConnectionStatus()){
+                deviceConnectionStatus.setText("Connected");
+            }
+            else {
+                deviceConnectionStatus.setText("Disconnected");
+            }
+
+
+            return convertView;
         }
 
-        SimpleExpandableListAdapter gattServiceAdapter = new SimpleExpandableListAdapter(
-                this,
-                gattServiceData,
-                android.R.layout.simple_expandable_list_item_2,
-                new String[] {LIST_NAME, LIST_UUID},
-                new int[] { android.R.id.text1, android.R.id.text2 },
-                gattCharacteristicData,
-                android.R.layout.simple_expandable_list_item_2,
-                new String[] {LIST_NAME, LIST_UUID},
-                new int[] { android.R.id.text1, android.R.id.text2 }
-        );
-        /*deviceListview.setAdapter(gattServiceAdapter);*/
     }
+
+    private void processResult(SmartBandage sm){
+        //Log.d(TAG,"New BLE Device:  " + result.getDevice().getName() + " @ " + result.getRssi());
+
+        msgHandler.sendMessage(Message.obtain(null,0,sm));
+    }
+    private Handler msgHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg){
+            SmartBandage smartBandage = (SmartBandage) msg.obj;
+            deviceConnectionStatus.put(smartBandage.getBandageAddress(), smartBandage);
+
+            connectionListAdapter.setNotifyOnChange(false);
+            connectionListAdapter.clear();
+            connectionListAdapter.addAll(deviceConnectionStatus.values());
+            connectionListAdapter.notifyDataSetChanged();
+        }
+    };
 }
--- app/src/main/java/com/example/jared/smart_bandage_android/SmartBandage.java
+++ app/src/main/java/com/example/jared/smart_bandage_android/SmartBandage.java
@@ -1,5 +1,6 @@
 package com.example.jared.smart_bandage_android;
 
+import android.bluetooth.BluetoothGatt;
 import android.bluetooth.le.ScanRecord;
 import android.os.ParcelUuid;
 
@@ -10,13 +11,20 @@ import java.io.Serializable;
  */
 public class SmartBandage implements Serializable{
     public static final ParcelUuid BANDAGE_SERVICE = ParcelUuid.fromString("0000f0f0-0000-1000-8000-00805f9b34fb");
-
+    public final static boolean CONNECTED = true;
+    public final static boolean DISCONNECTED = false;
     private String bandageName;
     private String bandageAddress;
-
+    private boolean bandageConnectionStatus = false;
     public SmartBandage(ScanRecord record,String bandageAddress) {
         this.bandageAddress = bandageAddress;
         this.bandageName = record.getDeviceName();
+    }
+
+    public SmartBandage(BluetoothGatt gatt,boolean connStatus) {
+        this.bandageAddress = gatt.getDevice().getAddress();
+        this.bandageName = gatt.getDevice().getName();
+        this.bandageConnectionStatus = connStatus;
     }
 
     public String getBandageName() {
@@ -33,6 +41,14 @@ public class SmartBandage implements Serializable{
 
     public void setBandageAddress(String bandageAddress) {
         this.bandageAddress = bandageAddress;
+    }
+
+    public void setBandageConnectionStatus(boolean status) {
+        this.bandageConnectionStatus = status;
+    }
+
+    public boolean getBandageConnectionStatus() {
+        return this.bandageConnectionStatus;
     }
 
 }
