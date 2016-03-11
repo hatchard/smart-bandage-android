449bcc9a292515966ee9d0d02814b0714d37fd65
hatchard <hatchard@ualberta.ca>
2016-03-11 14:18:19 -0700

    menu connect to new bandage directs to bluetooth scanner, mainActivity

--- app/src/main/java/com/example/jared/smart_bandage_android/DisplayBandageReadingsActivity.java
+++ app/src/main/java/com/example/jared/smart_bandage_android/DisplayBandageReadingsActivity.java
@@ -1,12 +1,14 @@
 package com.example.jared.smart_bandage_android;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
@@ -43,13 +45,44 @@ public class DisplayBandageReadingsActivity extends AppCompatActivity {
         return true;
     }
 
-        private ArrayList<DisplayModel> generateData(){
-            ArrayList<DisplayModel> models = new ArrayList<DisplayModel>();
-            models.add(new DisplayModel(R.drawable.thermometer,"Temperature: ","1")); //change to icon matching
-            models.add(new DisplayModel(R.drawable.cloud,"Humidity: ","2"));
-            models.add(new DisplayModel(R.drawable.raindrop,"Moisture: ","12"));
 
-            return models;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.new_connection:
+                viewNewConnection();
+                return true;
+            case R.id.advanced_view:
+                //viewAdvancedView();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
+
+
+    private ArrayList<DisplayModel> generateData(){
+        ArrayList<DisplayModel> models = new ArrayList<DisplayModel>();
+        models.add(new DisplayModel(R.drawable.thermometer,"Temperature: ","1")); //change to icon matching
+        models.add(new DisplayModel(R.drawable.cloud,"Humidity: ","2"));
+        models.add(new DisplayModel(R.drawable.raindrop,"Moisture: ","12"));
+
+        return models;
+    }
+
+    public void viewNewConnection() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    /*
+    public void viewAdvancedView(){
+        Intent intent = new Intent();
+        activity.startActivity(intent);
+    }
+    */
+
 }
 
